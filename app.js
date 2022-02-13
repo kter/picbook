b54d7c5d913c7bbded4d5afe4b5ebb2accf29fd8
Tomohiko Takahashi <takahashi@tomohiko.io>
2022-02-14 01:02:34 +0900

    アップロード画像が開けないがとりあえずコミット

--- app.js
+++ app.js
@@ -12,38 +12,63 @@ const app = new Vue({
     this.retrieveAndSetPicture()
   },
   methods: {
-    add: () => {
-      // upload photo
+    onFileChange: (event) => {
+      this.file = event.currentTarget.files[0]
+    },
+    add() {
+      // ********************
+      // *** UPLOAD PHOTO ***
+      // ********************
       let formData = new FormData()
-      formData.append('files[]', file)
-      axios.post('https://dtv8xadmd9.execute-api.ap-northeast-1.amazonaws.com/dev/img.pictures/3/', payload)
-      // upload data
-      const payload = {
-        'userId': app.userId,
-        'name': app.name,
-        'url': app.url,
-        'description': app.description
-      }
-      axios.post('https://dtv8xadmd9.execute-api.ap-northeast-1.amazonaws.com/dev', payload)
+      formData.append('file', file)
+      // get extension
+      fileExtension = file.type.replace(/image\//i, '')
+        axios({
+          method: 'PUT',
+          headers: {
+            'Content-Type': file.type,
+            'X-HTTP-Method-Override': 'PUT',
+          },
+          url: 'https://dtv8xadmd9.execute-api.ap-northeast-1.amazonaws.com/dev/img.pictures/' + app.userId + '_' + '01.' + fileExtension,
+          data: formData
+        })
         .then((response) => {
-          console.log(response)
+          console.debug(response)
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+        // *******************
+        // *** UPLOAD DATA ***
+        // *******************
+        const payload = {
+          'userId': app.userId,
+          'name': app.name,
+          'url': app.url,
+          'description': app.description
+        }
+        axios.post('https://dtv8xadmd9.execute-api.ap-northeast-1.amazonaws.com/dev', payload)
+        .then((response) => {
+          console.debug(response)
           this.retrieveAndSetPicture()
         })
         .catch((err) => {
-          alert(err.response.data.error)
+          console.error(err)
         })
     },
-    retrieveAndSetPicture: () => {
+    retrieveAndSetPicture() {
+      console.log('called retrieveAndSetPicture methods')
       const payload = {
         'userId': "3"
       }
       axios.get('https://dtv8xadmd9.execute-api.ap-northeast-1.amazonaws.com/dev', {params: payload})
         .then((response) => {
-          console.log(response)
-          app.pictures = response.data.Items || []
+          console.debug(response)
+          this.$data.pictures = response.data.Items || []
         })
         .catch((err) => {
-          alert(err.response.data.error)
+          // console.log(err.response.data.error)
+          console.error(err)
         })
     }
   }
